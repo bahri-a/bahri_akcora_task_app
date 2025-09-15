9efe3d09dbf2fe2b1d7abfc31f2f0cccdff4a00b
bahri-a <bahriakcora622@gmail.com>
2025-09-15 11:00:51 +0200

    4.5.1.1 & 4.5.2.2

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_2.dart
@@ -7,3 +7,14 @@ class S4522 extends StatelessWidget {
     throw UnimplementedError();
   }
 }
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text("Hello Bahri"), ElevatedButton(onPressed: () {}, child: Text('Klick mich'))],
+    );
+  }
+}
--- test/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1_test.dart
+++ test/solutions/chapter_4/subchapter_5/lesson_1/s_4_5_1_1_test.dart
@@ -3,8 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:task_app/solutions/solutions.dart';
 
 void main() {
-  testWidgets('S4511 should return a specific StatelessWidget',
-      (WidgetTester tester) async {
+  testWidgets('S4511 should return a specific StatelessWidget', (WidgetTester tester) async {
     await tester.pumpWidget(
       const MaterialApp(
         home: S4511(),
