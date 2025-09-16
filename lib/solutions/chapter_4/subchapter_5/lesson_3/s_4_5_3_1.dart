738eab78c176666f42bee16b9eb7845e36620675
bahri-a <bahriakcora622@gmail.com>
2025-09-16 18:20:54 +0200

    a

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_1.dart
@@ -4,6 +4,12 @@ class S4531 extends StatelessWidget {
   const S4531({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Column(
+      children: [
+        Container(height: 100, width: 100, color: Colors.blueGrey),
+        Container(height: 100, width: 100, color: Colors.blue),
+        Container(height: 100, width: 100, color: Colors.cyan)
+      ],
+    );
   }
 }
