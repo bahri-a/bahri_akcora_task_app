938e6f864cfed9d0903cfacd70f2865ec3eec7cd
bahri-a <bahriakcora622@gmail.com>
2025-09-18 15:29:33 +0200

    done 18.09.

--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_3.dart
@@ -4,6 +4,35 @@ class S4523 extends StatelessWidget {
   const S4523({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return MyNameWidget();
+  }
+}
+
+class MyNameWidget extends StatefulWidget {
+  MyNameWidget({super.key});
+
+  @override
+  State<MyNameWidget> createState() {
+    return MyNameWidgetState();
+  }
+}
+
+class MyNameWidgetState extends State<MyNameWidget> {
+  String text = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(children: [
+        Text(text, style: TextStyle(fontSize: 24)),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                text = "Bahri";
+              });
+            },
+            child: Text("Name anzeigen")),
+      ]),
+    );
   }
 }
--- lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_2/s_4_5_2_4.dart
@@ -4,6 +4,37 @@ class S4524 extends StatelessWidget {
   const S4524({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return ShowHideNameWidget();
+  }
+}
+
+class ShowHideNameWidget extends StatefulWidget {
+  ShowHideNameWidget({super.key});
+
+  @override
+  State<ShowHideNameWidget> createState() {
+    return ShowHideNameWidgetState();
+  }
+}
+
+class ShowHideNameWidgetState extends State<ShowHideNameWidget> {
+  String text = "";
+  String button = "Name anzeigen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(children: [
+        Text(text, style: TextStyle(fontSize: 24)),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                text == "" ? text = "Bahri" : text = "";
+                button == "Name anzeigen" ? button = "Name verstecken" : button = "Name anzeigen";
+              });
+            },
+            child: Text(button)),
+      ]),
+    );
   }
 }
