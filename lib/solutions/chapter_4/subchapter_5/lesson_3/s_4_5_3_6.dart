2ad0f2001958bafb2f0313ab613d3dacf79f8b1c
Bahri <bahriakcora622@gmail.com>
2025-09-22 21:26:54 +0200

    done

--- lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
+++ lib/solutions/chapter_4/subchapter_5/lesson_3/s_4_5_3_6.dart
@@ -4,6 +4,38 @@ class S4536 extends StatelessWidget {
   const S4536({super.key});
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    // List<Widget> tempProduct = [];
+
+    // for (Product p in items) {
+    //   tempProduct.add(ListTile(
+    //     title: Text(p.name),
+    //     subtitle: Text("${p.price}"),
+    //     leading: Icon(Icons.shopping_cart),
+    //   ));
+    // }
+
+    return ListView(
+        shrinkWrap: true,
+        children: items
+            .map((Product p) => ListTile(
+                  title: Text(p.name),
+                  subtitle: Text(p.price.toStringAsFixed(2)),
+                  leading: Icon(Icons.shopping_cart),
+                ))
+            .toList());
   }
 }
+
+class Product {
+  String name;
+  double price;
+  Product({required this.name, required this.price});
+}
+
+List<Product> items = [
+  Product(name: "Milch", price: 1.19),
+  Product(name: "Brot", price: 3.49),
+  Product(name: "Käse", price: 6.99),
+  Product(name: "Rotkohl", price: 1.49),
+  Product(name: "Weidebutter", price: 2.99),
+];
